5d6e7bfc534e2d83c8578c70d6b942a75541784e
Leks <quantorite@gmail.com>
2023-12-17 15:39:35 +0200

    Создар репозиторий libs и добавлены библиотеки, скачен pdfbox-app-3.0.1.jar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PDFToJPGConverter converter = new PDFToJPGConverter();
+        converter.convertPDFToJPG("C:\\Users\\quantum\\Downloads\\cat.pdf", "Преобразованные в JPG");
     }
 }
