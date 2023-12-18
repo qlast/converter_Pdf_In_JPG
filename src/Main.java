b7e605748f12ca3aec0f0cc87df17b05cc681126
Leks <quantorite@gmail.com>
2023-12-18 20:12:35 +0200

    // Путь к файлу

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         PDFToJPGConverter converter = new PDFToJPGConverter();
+        /**Путь к файлу пишется в формате "C:\\Users\\quantum\\Downloads\\cat.pdf"*/
         converter.convertPDFToJPG("C:\\Users\\quantum\\Downloads\\cat.pdf", "Преобразованные в JPG");
     }
 }
